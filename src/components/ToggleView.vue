8ce32d47249a8a8c7c6a586c112a6f5d640dcd83
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-06-25 19:11:47 +0300

    toggle view ready

--- src/components/ToggleView.vue
+++ src/components/ToggleView.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="search">
-    Toggle View
-  </div>
+ <div class="view">
+        <button
+          class='view__dashboard'>
+          <i class="fas fa-th-large"></i>
+        </button>
+        <button class='view__list'>
+         <i class="fas fa-bars"></i>
+        </button>
+      </div>
 </template>
 
 <script>
-
-// import Logo from '@/components/Logo.vue';
 
 export default {
   name: 'toggle-view',
@@ -16,4 +20,43 @@ export default {
 </script>
 
 <style>
+/* font-family:'FontAwesome' !important; */
+
+.view {
+  width: 100%;
+  height: 77px;
+  justify-content: flex-end;
+}
+
+.view__dashboard {
+  width: 40px;
+  justify-content: flex-end;
+  align-items: center;
+  cursor: pointer;
+  margin-right: 10px;
+  font-size: 32px;
+  color: #e0e0e0;
+}
+
+.view__list {
+  width: 40px;
+  justify-content: flex-end;
+  align-items: center;
+  cursor: pointer;
+  font-size: 36px;
+  color: #e0e0e0;
+}
+
+.view__dashboard.active,
+.view__list.active {
+  color: #3c4146;
+}
+
+.view__dashboard:focus,
+.view__list:focus {
+  outline: none;
+  border: none;
+  box-shadow: none;
+}
+
 </style>
