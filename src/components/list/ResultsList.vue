0b2d1dc10701538abaec0be13e9288501a094801
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-06-27 14:11:25 +0300

    deleted list view

--- src/components/list/ResultsList.vue
+++ src/components/list/ResultsList.vue
@@ -1,20 +1,28 @@
 <template>
-<ul class="results-list">
-        <li v-for='(value, key) in repos' :key='key' class="results-list__item">
-          <list-view :repo='value' />
-        </li>
-    </ul>
+  <ul class="results-list">
+    <li v-for='(value, key) in repos' :key='key' class="results-list__item">
+      <div class="repo">
+        <div class="wrapper-repo-action">
+          <button class='repo__action' />
+        </div>
+        <list-view-info />
+        <list-view-stats />
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script>
 
-import ListView from './ListView.vue';
+import ListViewInfo from './ListViewInfo.vue';
+import ListViewStats from './ListViewStats.vue';
 
 export default {
   name: 'results',
   props: ['repos'],
   components: {
-    ListView,
+    ListViewInfo,
+    ListViewStats,
   },
 };
 </script>
@@ -24,9 +32,27 @@ export default {
   flex-direction: column;
   width: 100%;
 }
-
 .results-list__item {
   height: 147px;
   border-top: 1px solid #e1e4e8;
+}
+.repo {
+  width: 100%;
+}
+.wrapper-repo-action {
+  padding-top: 32px;
+}
+.repo__action {
+  outline: 1px solid #0366d6;
+  border: 3px solid white;
+  height: 15px;
+  width: 15px;
+  cursor: pointer;
+}
+.repo__action.add {
+  background: white;
+}
+.repo__action.delete {
+  background: #0366d6;
 }
 </style>
